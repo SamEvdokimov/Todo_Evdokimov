8ae4335ededf889dd2419dfbd9dbd33225dd17bf
SamEvdokimov <s.evdokimov@tg.dunice.net>
2024-02-16 16:57:55 +0300

    adding handleClick

--- main.js
+++ main.js
@@ -3,6 +3,16 @@ const input = document.querySelector('#input');
 const list = document.querySelector('#list-for-render');
 const toDoList = [];
 
+const renderToDo = () => {
+  let listItems = '';
+  toDoList.forEach((todo) => {
+    if (todo.text !== '') {
+      listItems += `<li> <input type="checkbox"${todo.isChecked ? 'checked' : ''}/><span>${todo.text}</span> <button>Удалить</button> </li>`;
+    }
+  });
+  list.innerHTML = listItems;
+};
+
 const addNewTodo = (event) => {
   const toDo = {
     id: Date.now(),
@@ -11,21 +21,13 @@ const addNewTodo = (event) => {
   };
   toDoList.push(toDo);
   input.value = '';
-  let listItems = '';
-  toDoList.forEach((todo) => {
-    if (todo.text !== '') {
-      listItems += `<li> <input type="checkbox"${todo.isChecked ? 'checked' : ''}/><span>${todo.text}</span> <button>Удалить</button> </li>`;
-    }
-  });
-  list.innerHTML = listItems;
+  renderToDo();
   event.preventDefault();
 };
-
-sendButton.addEventListener('click', addNewTodo);
-
-/* searchField.addEventListener('keypress', function (e) {
-var key = e.which || e.keyCode;
-if (key === 13) { // код клавиши Enter
-sendButton.click();
+const handleClick = ((event) => {
+  console.log(event);
 }
-}); */
+);
+
+list.addEventListener('click', handleClick);
+sendButton.addEventListener('click', addNewTodo);
