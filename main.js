ff9c3b3dbd04f30a3f50ea3a41b6f06485c6b6fa
SamEvdokimov <s.evdokimov@tg.dunice.net>
2024-02-19 12:03:03 +0300

    checkAll

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-param-reassign */
 const sendButton = document.querySelector('#send-button');
 const input = document.querySelector('#input');
 const list = document.querySelector('#list-for-render');
-const toDoList = [];
+const checkTodos = document.querySelector('#check-all');
+let toDoList = [];
 
 const renderToDo = () => {
   let listItems = '';
@@ -25,9 +27,41 @@ const addNewTodo = (event) => {
   event.preventDefault();
 };
 const handleClick = ((event) => {
-  if (event.target.type === 'button') console.log('button');
-}
-);
+  if (event.target.type === 'checkbox') {
+    const li = event.target.closest('li');
 
+    const textElement = li.querySelector('span');
+
+    if (event.target.checked) {
+      textElement.style.textDecoration = 'line-through';
+    } else {
+      textElement.style.textDecoration = 'none';
+    }
+  }
+  if (event.target.type === 'submit') {
+    const idToDelete = parseInt(event.target.parentNode.dataset.id, 10);
+    toDoList = toDoList.filter((todo) => todo.id !== idToDelete);
+    renderToDo();
+  }
+});
+const checkAll = (event) => {
+  toDoList.forEach((todo) => {
+    todo.isChecked = event.target.checked;
+  });
+  renderToDo();
+  const textElements = document.querySelectorAll('#list-for-render span');
+  textElements.forEach((textElement, index) => {
+    if (toDoList[index].isChecked) {
+      textElement.style.textDecoration = 'line-through';
+    } else {
+      textElement.style.textDecoration = 'none';
+    }
+  });
+  /*  setTimeout(() => {
+    renderToDo();
+  }, 600); */
+};
+
+checkTodos.addEventListener('click', checkAll);
 list.addEventListener('click', handleClick);
 sendButton.addEventListener('click', addNewTodo);
