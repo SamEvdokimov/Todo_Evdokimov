1b640fc1099042f845c44984e537890d1ade644c
SamEvdokimov <s.evdokimov@tg.dunice.net>
2024-02-28 10:13:02 +0300

    added changes after code review

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 (() => {
+  const KEYDOWN_ENTER = 'Enter';
+  const KEYDOWN_ESC = 'Escape';
+  const TASKS_ON_PAGE = 5;
+  const DOUBLE_CLICK = 2;
+
   const sendButton = document.querySelector('#send-button');
   const deleteButton = document.querySelector('#delete-button-all');
   const inputNewTodo = document.querySelector('#input-todo-text');
@@ -7,27 +12,21 @@
   const groupButtons = document.querySelector('.group-buttons');
   const paginationPages = document.querySelector('.render-pagination');
 
-  const KEYDOWN_ENTER = 'Enter';
-  const KEYDOWN_ESC = 'Escape';
-  const TASKS_ON_PAGE = 5;
-  const DOUBLE_CLICK = 2;
-
   let currentPage = 1;
   let filterType = 'all-tasks';
   let toDoList = [];
 
-  const sliceTodo = (array) => {
+  const sliceTodo = (sliceTabTodo) => {
     const end = currentPage * TASKS_ON_PAGE;
     const start = end - TASKS_ON_PAGE;
-    return array.slice(start, end);
+    return sliceTabTodo.slice(start, end);
   };
 
-  const countPages = (array) => {
+  const countPages = (countTabTodo) => {
     let paginationButtons = '';
-    const pagesCount = Math.ceil(array.length / TASKS_ON_PAGE);
+    const pagesCount = Math.ceil(countTabTodo.length / TASKS_ON_PAGE);
     for (let i = 1; i <= pagesCount; i += 1) {
-      paginationButtons
-    += `<button ${i === currentPage ? 'class = "active"' : ''}>${i}</button>`;
+      paginationButtons += `<button ${i === currentPage ? 'class = "active"' : ''}>${i}</button>`;
     }
     paginationPages.innerHTML = paginationButtons;
   };
@@ -36,28 +35,25 @@
     checkTodos.checked = toDoList.length > 0 && toDoList.every((todo) => todo.isChecked);
   };
 
-  const renderTaskCount = () => {
+  const renderTodoCount = () => {
     const all = toDoList.length;
     const active = toDoList.filter((todo) => !todo.isChecked).length;
     const completed = toDoList.filter((todo) => todo.isChecked).length;
     const list = [all, active, completed];
     return list;
   };
-  const renderTasksButton = () => {
-    const all = renderTaskCount();
-    groupButtons.children[0].innerText = `All (${all[0]})`;
-    groupButtons.children[1].innerText = `Active (${all[1]})`;
-    groupButtons.children[2].innerText = `Completed (${all[2]})`;
+  const renderTodoButton = () => {
+    const all = renderTodoCount();
+    groupButtons.children[0].textContent = `All (${all[0]})`;
+    groupButtons.children[1].textContent = `Active (${all[1]})`;
+    groupButtons.children[2].textContent = `Completed (${all[2]})`;
   };
-  const tabButtonList = () => {
-    let filterTodo = [];
+  const tabButtonListFilter = () => {
     if (filterType === 'all-active-tasks') {
-      filterTodo = toDoList.filter((todo) => !todo.isChecked);
-      return filterTodo;
+      return toDoList.filter((todo) => !todo.isChecked);
     }
     if (filterType === 'all-completed-tasks') {
-      filterTodo = toDoList.filter((todo) => todo.isChecked);
-      return filterTodo;
+      return toDoList.filter((todo) => todo.isChecked);
     }
     return toDoList;
   };
@@ -77,30 +73,32 @@
     }
   };
   const renderToDo = () => {
-    const tabTodo = tabButtonList();
+    const filteredTodo = tabButtonListFilter();
     let listItems = '';
-    const currentTodo = sliceTodo(tabTodo);
+    const currentTodo = sliceTodo(filteredTodo);
     currentTodo.forEach((todo) => {
-      listItems
-      += `<li id="todo-list-item" data-id=${todo.id}> 
-      <div id="todo-text">
-      <input type="checkbox"${todo.isChecked ? 'checked' : ''}/>
-      <input class = "edit-input" hidden value="${todo.text}"/>
-      <span>${todo.text}</span>
-      </div>
-      <button>X</button> 
-      </li>`;
+      listItems += `
+      <li id="todo-list-item" data-id=${todo.id}>
+        <div id="todo-text">
+        <input type="checkbox"${todo.isChecked ? 'checked' : ''}/>
+        <input class="edit-input" hidden value="${todo.text}"/>
+        <span>${todo.text}</span>
+        </div>
+        <button>X</button> 
+      </li>
+      `;
     });
-    countPages(tabTodo);
-    renderTasksButton();
+    countPages(filteredTodo);
+    renderTodoButton();
     checkAllHandler();
     showActiveTab();
     listToDos.innerHTML = listItems;
   };
   const returnPreviousPage = () => {
-    const newTodolist = tabButtonList();
-    if (currentPage > Math.ceil(newTodolist.length / TASKS_ON_PAGE)) {
-      currentPage = Math.ceil(newTodolist.length / TASKS_ON_PAGE);
+    const newTodolist = tabButtonListFilter();
+    const allPages = Math.ceil(newTodolist.length / TASKS_ON_PAGE);
+    if (currentPage > allPages) {
+      currentPage = allPages;
     }
   };
   const showCounterType = (event) => {
@@ -117,13 +115,13 @@
   };
 
   const validation = (inputValue, previousText) => {
-    inputValue = inputValue.trim().replace(/(?:(?:^|\n)\s+|\s+(?:$|\n))/g, ' ').replace(/[^\w\s]/gi, '');
+    inputValue = inputValue.trim().replace(/(?:(?:^|\n)\s+|\s+(?:$|\n))/g, ' ').replace(/[^\w\s\u0400-\u04FF]/gi, '');
     if (inputValue !== '') {
       return inputValue;
     }
     return previousText;
   };
-  const addNewTodo = (event) => {
+  const addNewTodo = () => {
     const textTOdo = inputNewTodo.value;
     const sanitizedTodoText = validation(textTOdo, '');
     if (sanitizedTodoText !== '') {
@@ -137,7 +135,6 @@
       filterType = 'all-tasks';
       currentPage = Math.ceil(toDoList.length / TASKS_ON_PAGE);
       renderToDo();
-      event.preventDefault();
     }
   };
 
